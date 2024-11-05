9e056d1815f60b4afe5088de1e7f54d201d56ec9
BilTek <BilTek@HP>
2024-11-05 20:16:01 +0300

    Add project files.

---
+++ yazilimliste/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yazilimliste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> isimler = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isimler.Add(textBox1.Text);
+            MessageBox.Show("Kayıt Eklendi");
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach(string s in isimler)
+            {
+                listBox1.Items.Add(s);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int durum =isimler.IndexOf(textBox2.Text);
+            if (durum > -1)
+            {
+                MessageBox.Show("Kayıt Bulundu");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Bulunmadı");
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = listBox1.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int durum = isimler.IndexOf(textBox2.Text);
+            if (durum > -1)
+            {
+                isimler.Remove(textBox2.Text);
+                MessageBox.Show("Kayıt Bulundu");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Bulunmadı");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int durum = isimler.IndexOf(textBox2.Text);
+            textBox1.Focus();
+            textBox1.BackColor = Color.Bisque;
+            if (durum > -1)
+            {
+                isimler.Insert(durum,textBox1.Text);
+                isimler.Remove(textBox2.Text);
+                MessageBox.Show("Kayıt Bulundu");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Bulunmadı");
+            }
+        }
+    }
+}
